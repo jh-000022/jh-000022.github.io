4f164ba56917e61cb234bb337b9cff65c2d397a9
accountrev <55156874+accrev@users.noreply.github.com>
2023-01-11 19:25:25 -0500

    few

--- cyberlungs/cyberlungs.service.worker.js
+++ cyberlungs/cyberlungs.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1673475742|2418316721";
+const CACHE_VERSION = "1673483121|9796933009";
 const CACHE_PREFIX = "iosTest-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "cyberlungs.offline.html";
